92ac54bc804ffa6024497ede48578bf1d082397e
Sabbir Ahmed Tonmoy <tmtanmoy1234@gmail.com>
2023-03-02 22:38:22 +0600

    helo

--- js/app3.js
+++ js/app3.js
@@ -5,19 +5,19 @@ addBtn.addEventListener('click', () => {
     })
 
 
-const addNote = () => {
-const note = document.createElement('div');
-note.classList.add('note');
-note.innerHTML = `<div class="tool">
-<i class="saves fas fa-save"></i>
-<i class="remove fas fa-trash"></i>
-</div>
-<textarea></textarea>`;
-note.querySelector(".remove").addEventListener("click", () =>{
-    note.remove()
-});
-// note.querySelector('.save').addEventListener("click", () => {
-//     saveNote();
-// })
-main.appendChild(note)
-};
+// const addNote = () => {
+// const note = document.createElement('div');
+// note.classList.add('note');
+// note.innerHTML = `<div class="tool">
+// <i class="saves fas fa-save"></i>
+// <i class="remove fas fa-trash"></i>
+// </div>
+// <textarea></textarea>`;
+// note.querySelector(".remove").addEventListener("click", () =>{
+//     note.remove()
+// });
+// // note.querySelector('.save').addEventListener("click", () => {
+// //     saveNote();
+// // })
+// main.appendChild(note)
+// };
